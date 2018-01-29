8010e3ceb4956b324da69d8f54ac2c04e3025ffb
Ahmed Magdy <ahmad.magdy@nabdacare.com>
2018-01-29 12:45:12 +0200

    different shapes of loops

--- beginners/looping.sh
+++ beginners/looping.sh
@@ -4,3 +4,18 @@ do
 	echo $X
 	X=$((X+2))
 done
+
+for i in 1 2 3 4 5
+do
+   echo "Welcome $i times"
+done
+
+for i in {0..10..2}
+do
+   echo "Welcome $i times"
+done
+
+for i in $(seq 1 2 22)
+do
+   echo "Welcome $i times"
+done
