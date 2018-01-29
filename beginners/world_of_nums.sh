e3aa6acea04a5c0aaeb9e3ba81bf342d6cb355a6
Ahmed Magdy <ahmad.magdy@nabdacare.com>
2018-01-29 12:57:42 +0200

    basic operations over two nums

---
+++ beginners/world_of_nums.sh
@@ -0,0 +1,9 @@
+# https://www.hackerrank.com/challenges/bash-tutorials---the-world-of-numbers/problem
+
+read num1
+read num2
+
+echo "$(($num1+$num2))"
+echo "$(($num1-$num2))"
+echo "$(($num1*$num2))"
+echo "$(($num1/$num2))"
